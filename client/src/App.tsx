d2f7cb257205d3565c3ac4752b9dd1a89a7eecca
TahmidMubashira <tahmidobonti9@gmail.com>
2026-02-28 17:52:12 +0600

    added homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,17 @@
 import { Outlet, Route, Routes } from 'react-router';
-import BaseLayout from './views/BaseLayout';
-import Home from './views/Home';
+import HomePage from './views/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
-import Sessions from './views/Sessions';
+
 
 function App() {
   return (
     <>
       <Routes>
-        <Route
-          element={
-            <BaseLayout>
-              <Outlet />
-            </BaseLayout>
-          }
-        >
-          <Route path={'/'} element={<Home />} />
-          <Route path={'/sessions'} element={<Sessions />} />
-        </Route>
+        <Route path={'/'} element={<HomePage />} />
       </Routes>
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          error: {
-            duration: 5000,
-          },
-        }}
-      />
+      <Toaster position="top-center" />
     </>
   );
 }
