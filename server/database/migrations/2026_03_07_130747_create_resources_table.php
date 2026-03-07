dcbab0ef81cdae6b2c722dd79676324c5311e5bf
Shreon <tasmiashreoshi@gmail.com>
2026-03-07 19:18:28 +0600

    create resources table and model

---
+++ server/database/migrations/2026_03_07_130747_create_resources_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('department');
+            $table->string('courseCode');
+            $table->string('resourceType');
+            $table->string('file_path');
+            $table->string('file_type');
+            $table->string('file_size')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->integer('downloads')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
