5e90c5e052fef151988680385908faad87f239ea
TahmidMubashira <tahmidobonti9@gmail.com>
2026-03-07 17:26:42 +0600

    fixed authentication

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -40,11 +40,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-    ];
+             'throttle:api',
+              \Illuminate\Routing\Middleware\SubstituteBindings::class,
+],
+        ];
 
     /**
      * The application's route middleware.
