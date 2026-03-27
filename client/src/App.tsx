e2c68bd840f1cfd52b738ce0a070afd3a413e7cf
tasnimap <tasnimafarukprapty@gmail.com>
2026-03-27 19:43:18 +0600

    Rewards and upload page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import { Route, Routes } from 'react-router-dom'; // Change from 'react-router' to 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import StudentProfile from './views/StudentProfile';
 import AdminPage from './views/AdminPage';
 import ResourcesPage from './views/ResourcesPage';
 import { LoginPage, RegisterPage } from './views/Auth';
+import RewardsPage from './views/RewardsPage';
+import UploadPage from './views/Uploadpage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
@@ -18,7 +20,9 @@ function App() {
         <Route path="/resources" element={<ResourcesPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        {/* Add a catch-all route to redirect to home */}
+        <Route path="/rewards" element={<RewardsPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        {/* catch-all */}
         <Route path="*" element={<HomePage />} />
       </Routes>
       <Toaster position="top-center" />
